095fb13c8b1081fe4e28062d4a42b0cbf46f2255
Levent Sayginsoy <sayginsoy1978@gmail.com>
2026-01-30 20:40:48 +0100

    Update Dashboard

--- githubService.ts
+++ githubService.ts
@@ -1,6 +1,5 @@
 export const getGitHubConfig = (): GitHubConfig => {
   return {
-    // Diese Befehle sagen dem Programm: "Schau in den Vercel-Tresor"
     token: import.meta.env.VITE_GITHUB_TOKEN || '',
     owner: import.meta.env.VITE_GITHUB_OWNER || '',
     repo: import.meta.env.VITE_GITHUB_REPO || '',
