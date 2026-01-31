3ea8c41eb34bc4bc56721ab759ff72ddff423850
Levent Sayginsoy <sayginsoy1978@gmail.com>
2026-01-31 10:43:22 +0100

    Update Dashboard

--- admin/admin.js
+++ admin/admin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AdminApp from './AdminApp.tsx';
+import AdminApp from './AdminApp.js';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -9,7 +9,7 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <AdminApp />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(AdminApp)
+  )
 );
